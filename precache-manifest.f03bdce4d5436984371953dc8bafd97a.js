1042dfe72668d9704e066d27a5474a67c737aa96
shanejix <shanejix@vip.163.com>
2019-08-09 23:18:10 +0800

    finished

---
+++ precache-manifest.f03bdce4d5436984371953dc8bafd97a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f452e69c8e25483f55d18fb4f8233b50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae5f7e555cf0250b49d0",
+    "url": "/static/css/2.2974b717.chunk.css"
+  },
+  {
+    "revision": "feec9b2ac9c33b9b7930",
+    "url": "/static/css/main.dd4fc9f9.chunk.css"
+  },
+  {
+    "revision": "ae5f7e555cf0250b49d0",
+    "url": "/static/js/2.ab7e92eb.chunk.js"
+  },
+  {
+    "revision": "feec9b2ac9c33b9b7930",
+    "url": "/static/js/main.2250b252.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bcb3c55a0fe768b22696132ea1185bde",
+    "url": "/static/media/cover.bcb3c55a.jpg"
+  },
+  {
+    "revision": "b895601d8fd4b4f8ab4a79d26090bbaf",
+    "url": "/static/media/user.b895601d.jfif"
+  }
+]);
