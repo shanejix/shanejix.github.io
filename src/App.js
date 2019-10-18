b46325b7d7f64268d30e953bffaa559246841105
shanejix <shanejix@vip.163.com>
2019-10-18 22:09:07 +0800

    init create-react-app-dev

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route,Redirect } from 'react-router-dom'
+import logo from './logo.svg';
+import './App.css';
 
-import Layouts from './layouts'
-
-import Home from './pages/home'
-import About from './pages/about'
-import Archive from './pages/archive'
-import Articles from './pages/articles'
-import Message from './pages/message'
-import Timeline from './pages/timeline'
-import Detail from './pages/detail'
-
-class App extends React.Component {
-	render() {
-		return (
-			<BrowserRouter>
-				<Layouts>
-					<Switch>
-						<Route path='/' exact component={Home} />
-						<Route path='/about' exact component={About} />
-						<Route path='/archive' exact component={Archive} />
-						<Route path='/articles' exact component={Articles} />
-						<Route path='/message' exact component={Message} />
-						<Route path='/timeline' exact component={Timeline} />
-						<Route path='/detail' exact component={Detail} />
-						<Redirect to='/' />	
-					</Switch>
-				</Layouts >
-			</BrowserRouter >
-		);
-	}
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { Provider } from 'react-redux'
-
-import store from './redux/store'
-
+import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
+ReactDOM.render(<App />, document.getElementById('root'));
 
-
-
-// let store = createStore(reducer, applyMiddleware(reduxThunk))
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-    ,
-    document.getElementById('root')
-);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
