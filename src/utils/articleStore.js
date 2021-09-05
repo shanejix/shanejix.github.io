877dd9ca731a964b08bb1843d35785f2370c8741
shanejix <shanejix@hotmail.com>
2021-09-05 23:40:22 +0800

    init

--- src/utils/articleStore.js
+++
@@ -1,8 +0,0 @@
-export let  getItem = (key='article')=> {
-    return JSON.parse(localStorage.getItem(key))
-}
-
-export let saveItem = (data) => {
-
-    localStorage.setItem('article',JSON.stringify(data)||{})
-}
